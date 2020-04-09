3961af224dcf9d77cde973ba26931f9710c04bcd
zachar papkov <z.papkov@gmail.com>
2020-04-10 01:10:05 +0300

    Robot_Mgmt_protocol

--- Arcadia4/robot_comm.py
+++ Arcadia4/robot_comm.py
@@ -2,16 +2,6 @@ import asyncio
 import websockets
 
 
-async def robot_management_protocol(websocket, path):
-    async for message in websocket:
-        print(f"< {message}")
-
-        greeting = f"Hello {message}!"
-
-        await websocket.send(greeting)
-        print(f"> {greeting}")
-
-
 class RobotComm:
     def __init__(self, ip_address, port):
         self.ip_address = ip_address
@@ -20,7 +10,18 @@ class RobotComm:
     def details_server_param(self):
         print("robot server begin " + self.ip_address, self.port)
 
+    async def Robot_Mgmt_protocol(self, websocket, path):
+        async for message in websocket:
+            print(f"< {message}")
+
+            greeting = f"Hello {message}!"
+
+            await websocket.send(greeting)
+            print(f"> {greeting}")
+
     def  start_robot_comm(self):
-        start_server = websockets.serve(robot_management_protocol, self.ip_address, self.port)
+        start_server = websockets.serve(self.Robot_Mgmt_protocol, self.ip_address, self.port)
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_forever()
+
+
